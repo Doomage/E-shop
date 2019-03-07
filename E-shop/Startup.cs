cb35ad054ed6d7322683ab0a466a6362bdb74cb0
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2019-03-07 18:24:50 +0200

    Add project files.

---
+++ E-shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_shop.Startup))]
+namespace E_shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
